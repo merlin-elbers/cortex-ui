0893ef846bc7a3f6087ce7841eb8a68c7cc59af7
merlin-elbers <merlin@elbers.dev>
2025-07-25 23:18:16 +0200

    refactor: removed unused import statement

--- components/setup/BrandingStep.tsx
+++ components/setup/BrandingStep.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useRef} from 'react';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
